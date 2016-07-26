322927ccad91c37218c2f12f8b4b53be33c8f56c
paul <affentranger@afca.ch>
2016-07-26 20:39:41 +0200

    Adaptation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { GOOGLE_MAPS_DIRECTIVES } from 'angular2-google-maps/core';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [GOOGLE_MAPS_DIRECTIVES]
 })
 export class AppComponent {
   title = 'app works!';
+  lat: number = 51.678418;
+  lng: number = 7.809007;
+  ishidden: boolean = true;
+
+  switch(){
+  this.ishidden = !this.ishidden;
+  }
 }
+
+
